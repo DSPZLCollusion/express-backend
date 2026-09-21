56db6814e347e71979af2216de77233514f13a7e
JJue888 <joshuajue888@gmail.com>
2026-09-21 11:04:56 -0700

    fix: support multiple allowed origins in CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const allowedOrigins = [
+    'https://dspzlcollusion.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:8080'
+];
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://dspzlcollusion.vercel.app');
+    const origin = req.headers.origin;
+    if (origin && (allowedOrigins.includes(origin) || /^http:\/\/localhost:\d+$/.test(origin) || /^http:\/\/127\.0\.0\.1:\d+$/.test(origin))) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', 'https://dspzlcollusion.vercel.app');
+    }
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
